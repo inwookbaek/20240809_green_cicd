117873da5b000286005f00824cef4c4ed8d526d2
inwookbaek <iwbaek@gmail.com>
2024-12-24 16:14:33 +0900

    20241224

---
+++ src/main/java/com/lec/board/Sp02DockerApplication.java
@@ -0,0 +1,13 @@
+package com.lec.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp02DockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp02DockerApplication.class, args);
+	}
+
+}
