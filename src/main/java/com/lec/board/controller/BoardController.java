8756faf7794e5a45b7592554b64b03b65102b18f
GILBAEK <42198744+inwookbaek@users.noreply.github.com>
2025-01-02 13:31:49 +0900

    Update BoardController.java

--- src/main/java/com/lec/board/controller/BoardController.java
+++ src/main/java/com/lec/board/controller/BoardController.java
@@ -10,7 +10,7 @@ public class BoardController {
 
 	@GetMapping("/list")
 	public String list() {
-		return "<h1 align=\"center\">최종테스트 2nd! - Board List....</h1>";
+		return "<h1 align=\"center\">최종테스트 3rd! - Board List....</h1>";
 	}
 	
 	@GetMapping("/getBoard")
